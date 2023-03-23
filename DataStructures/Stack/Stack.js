e2d2d0390d756294e5bfc869ca1d43dbaa8ea19b
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-03-23 10:23:41 +0200

    Fixed bug where didn't show properly the last item in stack

--- DataStructures/Stack/Stack.js
+++ DataStructures/Stack/Stack.js
@@ -25,20 +25,38 @@ class Stack {
     }
 
     pop() {
-        this.length = Math.max(0, this.length - 1); // To don't go into negative length
+        this.length = Math.max(0, this.length - 1);
+
         if (this.length === 0) {
-            this.head = undefined;
-            return this.head?.value;
+            const head = this.head;
+            this.head = undefined; // Sets it for the next pop it will be undefined;
+            return head?.value;
         }
+
         const head = this.head;
         this.head = head.prev;
 
-        return head.value;
+        return head?.value;
     }
 
     peek() {
         return this.head?.value;
     }
 }
+
+// const st = new Stack();
+
+// st.push(10);
+// st.push(20);
+// st.push(30);
+
+// // console.log(st.head.value); // 30 because head is the most recently added
+
+// console.log(st.pop());
+// console.log(st.pop());
+
+// console.log(st.pop()); // should return 10 but it returns undefined
+
+// console.log(st.pop()); // should be undefined
 
 module.exports = { Stack };
