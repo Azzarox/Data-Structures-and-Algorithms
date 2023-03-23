8460950c305e9dc15bad7287fddb9f19306139e3
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-03-23 09:59:05 +0200

    Added Queue Data Structure implementation

---
+++ DataStructures/Queue/Queue.js
@@ -0,0 +1,48 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = undefined;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
+    }
+
+    enqueue(value) {
+        const node = new Node(value);
+        this.length++;
+
+        if (!this.head) {
+            this.head = this.tail = node;
+        }
+
+        this.tail.next = node;
+        this.tail = node;
+    }
+
+    deque() {
+        if (!this.head) {
+            return undefined;
+        }
+
+        this.length--;
+
+        const head = this.head;
+        this.head = this.head.next;
+
+        if (this.length === 0) {
+            this.tail = undefined;
+        }
+
+        return head.value;
+    }
+
+    peek() {
+        return this.head?.value;
+    }
+}
+
+module.exports = { Queue };
