394f3f136cca4579e4cfd21f76362989157c68d9
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-04-05 10:38:59 +0300

    Added BFS Traversal algorithm

---
+++ Other/BFSTraversal_BinaryTree/BFSTraversal_BinaryTree.js
@@ -0,0 +1,25 @@
+const { expect } = require('chai');
+const { BinaryTree } = require('../../DataStructures/BinaryTree/BinaryTree');
+
+const bfsTraverse = (tree) => {
+    const root = tree.root;
+
+    const queue = [root];
+    const result = [];
+
+    while (queue.length) {
+        const current = queue.shift();
+        if (!current) {
+            continue;
+        }
+
+        result.push(current.value);
+
+        queue.push(current.left);
+        queue.push(current.right);
+    }
+
+    return result.join(' ');
+};
+
+module.exports = { bfsTraverse };
