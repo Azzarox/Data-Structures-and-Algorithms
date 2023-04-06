67bce01a75702389e361b97583da644c1cd125eb
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-04-06 13:38:47 +0300

    Added tree since BinaryTree doesn't work correctly

---
+++ utils/tree.js
@@ -0,0 +1,31 @@
+const tree = {
+    value: 7,
+    right: {
+        value: 3,
+        right: {
+            value: 21,
+            right: null,
+            left: null,
+        },
+        left: {
+            value: 18,
+            right: null,
+            left: null,
+        },
+    },
+    left: {
+        value: 23,
+        right: {
+            value: 4,
+            right: null,
+            left: null,
+        },
+        left: {
+            value: 5,
+            right: null,
+            left: null,
+        },
+    },
+};
+
+module.exports = { tree };
